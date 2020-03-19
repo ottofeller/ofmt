c74832f8f2ea6a4ec977fb58cc5c291b5d736ccf
Artem Rudenko <gvidon@ottofeller.com>
2020-03-19 14:48:50 +0100

    Exclude ramda from min var length rule

--- base.js
+++ base.js
@@ -132,7 +132,7 @@ module.exports = {
             },
         }],
 
-        'id-length': ['error', {'exceptions': ['_']}],
+        'id-length': ['error', {'exceptions': ['_', 'R']}],
         'strict'   : [1, 'never'],
         'quotes'   : [1, 'single'],
         'eqeqeq'   : 2,
