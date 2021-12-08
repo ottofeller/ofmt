e48009189280e050566a20442f0d18a75e3a515f
Serator <agafvv@gmail.com>
2021-12-08 16:56:01 +0300

    Codestyle

--- eslint.base.js
+++ eslint.base.js
@@ -1,49 +1,42 @@
 module.exports = {
-  'extends': ['plugin:import/typescript'],
-  'globals': {'document': 'readonly', 'navigator': 'readonly', 'window': 'readonly'},
+  extends: ['plugin:import/typescript'],
+  globals: {document: 'readonly', navigator: 'readonly', window: 'readonly'},
 
-  'overrides': [
+  overrides: [
     {
       // Disable some rules for tests
-      'files': ['**/__tests__/*'],
+      files: ['**/__tests__/*'],
 
-      'rules': {
+      rules: {
         'import/no-relative-parent-imports': 'off',
       },
     },
   ],
 
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {'ecmaFeatures': {'jsx': true}, 'ecmaVersion': 2020, 'sourceType': 'module'},
+  parser: '@typescript-eslint/parser',
+  parserOptions: {ecmaFeatures: {jsx: true}, ecmaVersion: 2020, sourceType: 'module'},
+  plugins: ['@typescript-eslint', 'fp', 'import', 'ramda', 'unicorn'],
 
-  'plugins': [
-    '@typescript-eslint',
-    'fp',
-    'import',
-    'ramda',
-    'unicorn',
-  ],
-
-  'rules': {
+  rules: {
     '@typescript-eslint/array-type': ['error', {default: 'generic', readOnly: 'generic'}],
-    '@typescript-eslint/member-delimiter-style': ['warn', {'multiline': {'delimiter': 'none', 'requireLast': true}, 'singleline': {'delimiter': 'comma', 'requireLast': false}}],
-    '@typescript-eslint/no-unused-vars': ['error', {'argsIgnorePattern': '^_'}],
+    '@typescript-eslint/member-delimiter-style': ['warn', {multiline: {delimiter: 'none', requireLast: true}, singleline: {delimiter: 'comma', requireLast: false}}],
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
     'arrow-parens': ['error', 'as-needed'],
     'capitalized-comments': 'error',
-    'comma-dangle': ['error', {'arrays': 'always-multiline', 'exports': 'always-multiline', 'functions': 'always-multiline', 'imports': 'always-multiline', 'objects': 'always-multiline'}],
+    'comma-dangle': ['error', {arrays: 'always-multiline', exports: 'always-multiline', functions: 'always-multiline', imports: 'always-multiline', objects: 'always-multiline'}],
     'curly': ['error', 'all'],
     'eol-last': 'warn',
     'eqeqeq': 'error',
     'fp/no-loops': 'error',
-    'id-length': ['error', {'exceptions': ['_', 'R', 'x', 'y']}],
+    'id-length': ['error', {exceptions: ['_', 'R', 'x', 'y']}],
     'implicit-arrow-linebreak': ['error', 'beside'],
     'import/no-relative-parent-imports': 'error',
-    'indent': ['error', 2, {'ArrayExpression': 1, 'CallExpression': {'arguments': 1}, 'FunctionExpression': {'parameters': 1}, 'MemberExpression': 1, 'ObjectExpression': 1, 'VariableDeclarator': 0}],
-    'key-spacing': ['error', {'afterColon': true, 'align': 'colon'}],
-    'keyword-spacing': ['error', {'after': false, 'before': false, 'overrides': {'as': {'after': true, 'before': true}, 'catch': {'after': false, 'before': true}, 'const': {'after': true, 'before': false}, 'default': {'after': true, 'before': true}, 'export': {'after': true, 'before': false}, 'finally': {'after': true, 'before': true}, 'from': {'after': true, 'before': true}, 'import': {'after': true, 'before': false}, 'let': {'after': true, 'before': false}, 'of': {'after': true, 'before': true}, 'return': {'after': true, 'before': false}, 'try': {'after': true, 'before': false}, 'var': {'after': true, 'before': false}}}],
-    'line-comment-position': ['warn', {'position': 'above'}],
-    'lines-around-comment': ['error', {'allowBlockStart': true, 'beforeBlockComment': true, 'beforeLineComment': true}],
-    'max-len': ['error', {'code': 120, 'ignoreUrls': true, 'ignoreComments': true, 'tabWidth': 2}],
+    'indent': ['error', 2, {ArrayExpression: 1, CallExpression: {arguments: 1}, FunctionExpression: {parameters: 1}, MemberExpression: 1, ObjectExpression: 1, VariableDeclarator: 0}],
+    'key-spacing': ['error', {afterColon: true, align: 'colon'}],
+    'keyword-spacing': ['error', {after: false, before: false, overrides: {as: {after: true, before: true}, catch: {after: false, before: true}, const: {after: true, before: false}, default: {after: true, before: true}, export: {after: true, before: false}, finally: {after: true, before: true}, from: {after: true, before: true}, import: {after: true, before: false}, let: {after: true, before: false}, of: {after: true, before: true}, return: {after: true, before: false}, try: {after: true, before: false}, var: {after: true, before: false}}}],
+    'line-comment-position': ['warn', {position: 'above'}],
+    'lines-around-comment': ['error', {allowBlockStart: true, beforeBlockComment: true, beforeLineComment: true}],
+    'max-len': ['error', {code: 120, ignoreUrls: true, ignoreComments: true, tabWidth: 2}],
     'max-lines': ['warn', 300],
     'max-params': ['warn', 2],
     'no-alert': 'error',
@@ -54,7 +47,7 @@ module.exports = {
     'no-mixed-spaces-and-tabs': 'warn',
 
     // Allow for spaced alignemnt of "=" and ":"
-    'no-multi-spaces': ['error', {'exceptions': {'Property': true, 'AssignmentExpression': true}}],
+    'no-multi-spaces': ['error', {exceptions: {Property: true, AssignmentExpression: true}}],
 
     'no-multiple-empty-lines': ['error', {max: 1}],
     'no-negated-condition': 'error',
@@ -68,7 +61,7 @@ module.exports = {
     'one-var-declaration-per-line': ['error', 'always'],
     'one-var': ['error', 'never'],
     'padded-blocks': ['error', 'never'],
-    'padding-line-between-statements': ['error', {'blankLine': 'always', 'prev': '*', 'next': ['if', 'multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var']}, {'blankLine': 'always', 'prev': ['if', 'multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var'], 'next': '*'}],
+    'padding-line-between-statements': ['error', {blankLine: 'always', prev: '*', next: ['if', 'multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var']}, {blankLine: 'always', prev: ['if', 'multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var'], next: '*'}],
     'quotes': ['warn', 'single'],
     'ramda/compose-simplification': 'error',
     'ramda/cond-simplification': 'error',
@@ -84,11 +77,11 @@ module.exports = {
     'ramda/set-simplification': 'error',
     'ramda/when-simplification': 'error',
     'semi': ['error', 'never'],
-    'sort-imports': ['error', {'ignoreCase': true, 'ignoreMemberSort': false, 'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']}],
-    'sort-keys': ['error', 'asc', {'caseSensitive': false}],
-    'space-before-function-paren': ['warn', {'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always'}],
+    'sort-imports': ['error', {ignoreCase: true, ignoreMemberSort: false, memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']}],
+    'sort-keys': ['error', 'asc', {caseSensitive: false}],
+    'space-before-function-paren': ['warn', {anonymous: 'always', named: 'never', asyncArrow: 'always'}],
     'spaced-comment': ['error', 'always'],
     'strict': ['warn', 'never'],
-    'unicorn/prevent-abbreviations': ['error', {'replacements': {'dir': false, 'dirs': false, 'params': false, 'props': false}}],
+    'unicorn/prevent-abbreviations': ['error', {replacements: {dir: false, dirs: false, params: false, props: false}}],
   },
 }
--- eslint.react.js
+++ eslint.react.js
@@ -1,21 +1,16 @@
 module.exports = {
-  'env': {'browser': true, 'jest': true, 'node': false},
-  'extends': './eslint.base.js',
+  env: {browser: true, jest: true, node: false},
+  extends: './eslint.base.js',
+  plugins: ['@ottofeller/ottofeller', 'react-hooks', 'react'],
 
-  'plugins': [
-    '@ottofeller/ottofeller',
-    'react-hooks',
-    'react',
-  ],
-
-  'rules': {
+  rules: {
     '@ottofeller/ottofeller/jsx-newline-block': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
-    'react/destructuring-assignment': ['warn', 'never', {'ignoreClassFields': true}],
+    'react/destructuring-assignment': ['warn', 'never', {ignoreClassFields: true}],
     'react/jsx-curly-spacing': ['warn', 'never'],
     'react/jsx-fragments': ['error', 'element'],
-    'react/jsx-key': ['error', {'checkFragmentShorthand': true}],
+    'react/jsx-key': ['error', {checkFragmentShorthand: true}],
     'react/jsx-no-bind': 'error',
     'react/jsx-no-useless-fragment': 'error',
     'react/jsx-uses-vars': 'warn',
@@ -24,5 +19,5 @@ module.exports = {
     'react/self-closing-comp': 'error',
   },
 
-  'settings': {'react': {'pragma': 'React', 'version': '16.10'}},
+  settings: {react: {pragma: 'React', version: '16.10'}},
 }
