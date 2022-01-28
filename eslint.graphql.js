3f71e8e1f7af88b9509f6df04177f0eeb919cb5f
Serator <agafvv@gmail.com>
2022-01-28 18:35:34 +0300

    Add `eslint.graphql.js`

---
+++ eslint.graphql.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len -- disabling file length limitation for compactness. */
+module.exports = {
+  extends: ['./eslint.base.js'],
+  plugins: ['graphql'],
+
+  rules: {
+    'graphql/named-operations': ['error', {env: 'apollo', schemaJsonFilepath: './schema.json'}],
+    'graphql/required-fields' : ['error', {env: 'apollo', requiredFields: ['id', '_id', 'uuid'], schemaJsonFilepath: './schema.json'}],
+    'graphql/template-strings': ['error', {env: 'apollo', schemaJsonFilepath: './schema.json'}],
+  },
+}
