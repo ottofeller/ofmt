3529ec4ab921d3a1680b567b524d3604a1427901
Artem Rudenko <gvidon@ottofeller.com>
2022-04-07 13:53:02 +0200

    Format the bins with prettier

--- bin/ofmt.js
+++ bin/ofmt.js
@@ -4,28 +4,27 @@ import meow from 'meow'
 import util from 'util'
 const execAsync = util.promisify(exec)
 
-void async function () {
+void (async function () {
   const args = meow({
     importMeta: import.meta,
-    flags     : {'lint': {type: 'boolean', alias: 'l'}},
+    flags: {lint: {type: 'boolean', alias: 'l'}},
   })
 
-  const prettierOptions = ```
-    --print-width 120
-    --tab-width 2
-    --no-semi
-    --single-quote
-    --quote-props as-needed
-    --trailing-comma all
-    --no-bracket-spacing
-    --arrow-parens always
-```
+  const prettierOptions =
+    '--print-width 120 --tab-width 2 --no-semi --single-quote --quote-props as-needed --trailing-comma all --no-bracket-spacing --arrow-parens always'
 
-  if(args.flags.lint) {
+  console.log(prettierOptions)
+
+  if (args.flags.lint) {
     await execAsync(`npx prettier --check ${prettierOptions} ${args.input}`)
+    return
   }
 
-  if(!args.flags.lint) {
+  if (!args.flags.lint) {
+    console.log('Formatting...')
     await execAsync(`npx prettier --write ${prettierOptions} ${args.input}`)
+    return
   }
-}()
+
+  return
+})()
