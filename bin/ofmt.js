04005dfc64124f5bde4f6dea8fdff4c7bd31d8f5
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-04-13 20:59:56 +0300

    added prettier config file and removed CLI options from the calls

--- bin/ofmt.js
+++ bin/ofmt.js
@@ -10,14 +10,11 @@ void (async function () {
     flags: {lint: {type: 'boolean', alias: 'l'}},
   })
 
-  const prettierOptions =
-    '--print-width 120 --tab-width 2 --no-semi --single-quote --quote-props as-needed --trailing-comma all --no-bracket-spacing --arrow-parens always'
-
   if (args.flags.lint) {
-    await execAsync(`npx prettier --check ${prettierOptions} ${args.input}`)
+    await execAsync(`npx prettier --check ${args.input}`)
   }
 
   if (!args.flags.lint) {
-    await execAsync(`npx prettier --write ${prettierOptions} ${args.input}`)
+    await execAsync(`npx prettier --write ${args.input}`)
   }
 })()
