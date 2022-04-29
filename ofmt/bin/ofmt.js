53fbd028386aee2f81ebbdfa9c96436b41fc85c7
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-04-29 10:28:24 +0300

    updated references to configs
    provided version update for all packages in CI

--- ofmt/bin/ofmt.js
+++ ofmt/bin/ofmt.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 import {exec} from 'child_process'
 import meow from 'meow'
-import {fileURLToPath} from 'url'
 import path from 'path'
-
+import {fileURLToPath} from 'url'
 // eslint-disable-next-line import/no-relative-parent-imports -- Can't add a path in an easy way at the moment
 import {install} from '../lib/install.js'
 
@@ -18,6 +17,6 @@ if (args.input[0] === 'install') {
   install(args.input[1] || './')
 } else {
   const checkOrWrite = args.flags.lint ? 'check' : 'write'
-  const configPath = path.resolve(__dirname, '../../prettier-config-ofmt/index.json')
+  const configPath = path.resolve(__dirname, '../node_modules/@ottofeller/prettier-config-ofmt/index.json')
   exec(`npx prettier --${checkOrWrite} --config ${configPath} ${args.input}`, console.log)
 }
