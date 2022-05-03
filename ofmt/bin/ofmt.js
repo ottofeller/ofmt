7057e5482eed109bc4b5270cbf999f8f4ece3770
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-05-03 10:38:14 +0300

    added search logic for config path

--- ofmt/bin/ofmt.js
+++ ofmt/bin/ofmt.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import {exec} from 'child_process'
+import {statSync} from 'fs'
 import meow from 'meow'
 import path from 'path'
 import {fileURLToPath} from 'url'
@@ -7,6 +8,17 @@ import {fileURLToPath} from 'url'
 import {install} from '../lib/install.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const packageRoot = path.resolve(__dirname, '..')
+
+const prettierConfigPath = [
+  'node_modules/@ottofeller/prettier-config-ofmt/index.json', // Path within this package.
+  '../prettier-config-ofmt/index.json', // Path within another package, where 'ofmt' is installed as a dependency.
+].find((filePath) => statSync(path.resolve(packageRoot, filePath), {throwIfNoEntry: false}))
+
+if (!prettierConfigPath) {
+  console.error('Prettier config file is nod found. Please install "@ottofeller/prettier-config-ofmt".')
+  process.exit(1)
+}
 
 const args = meow({
   importMeta: import.meta,
@@ -17,6 +29,6 @@ if (args.input[0] === 'install') {
   install(args.input[1] || './')
 } else {
   const checkOrWrite = args.flags.lint ? 'check' : 'write'
-  const configPath = path.resolve(__dirname, '../node_modules/@ottofeller/prettier-config-ofmt/index.json')
+  const configPath = path.resolve(packageRoot, prettierConfigPath)
   exec(`npx prettier --${checkOrWrite} --config ${configPath} ${args.input}`, console.log)
 }
